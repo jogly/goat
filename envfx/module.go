0c0e6b93679263012f232b4626e515cc38ad79d4
Joseph Gilley <j@banditml.com>
2020-08-04 13:53:32 -0700

    add version environment variable to logs for datadog reporting (#1)

--- envfx/module.go
+++ envfx/module.go
@@ -2,6 +2,7 @@
 package envfx
 
 import (
+	"os"
 	"strings"
 
 	env "github.com/Netflix/go-env"
@@ -10,17 +11,18 @@ import (
 	"go.uber.org/zap"
 )
 
-// Mode lets consumers know what mode the app in running in.
+// Mode indicates what mode the app in running in. This should only be used
+// when setting up integration stubs/replacements.
 type Mode = string
 
 const (
-	// Test is the test Mode.
-	Test Mode = "test"
-	// Local is the local Mode.  There should be minimal
-	// dependency on this.
+	// Local mode is set when running the application locally.
 	Local Mode = "local"
-	// Production is a singular "released" Mode.  Staging is not
-	// distinct from Production.
+	// Test mode is set when running application tests.
+	Test Mode = "test"
+	// Release mode is set when running the application remotely, including
+	// `dev` and `prod` environments.  `Env.Env` indicates which _environment_,
+	// such as `dev` or `prod`.
 	Release Mode = "release"
 
 	DefaultEnv = "local"
@@ -28,10 +30,18 @@ const (
 
 // Env is a collection of configuration sourced from environment variables.
 type Env struct {
+	// Env can be local, test, dev, or prod.
 	Env  string `env:"ENV" validate:"required"`
 	Mode Mode
+
+	// Port is the port the application is listening on.
 	Port int `env:"PORT"`
 
+	// Version is the (likely) the git SHA.
+	Version string `env:"VERSION"`
+
+	// Postgres has the configuration for connecting to a PG db.
+	// Not used in local and test modes.
 	Postgres struct {
 		User     string `env:"PGUSER"`
 		Password string `env:"PGPASSWORD"`
@@ -39,9 +49,12 @@ type Env struct {
 		Database string `env:"PGDATABASE"`
 	}
 
+	// Everything else for debugging purposes.  `json:"-"` to never serialize,
+	// it has secrets.
 	Extras env.EnvSet `json:"-"`
 }
 
+// IsRelease is true if the application is running in the cloud.
 func (e *Env) IsRelease() bool {
 	return e.Mode == Release
 }
@@ -49,15 +62,30 @@ func (e *Env) IsRelease() bool {
 // Module is a FX Module
 var Module = fx.Provide(NewEnv)
 
-// NewEnv creates an Env from the OS environment.
-func NewEnv() *Env {
+// NewEnv creates an Env from the OS environment.  `overrides` allow testing
+// settings without modifying the global OS environment.
+func NewEnv(overrides ...env.ChangeSet) *Env {
+	// create a temporary console logger to use because the application logger
+	// is dependent on the struct we're creating here.
 	l, _ := zap.NewDevelopment()
+	// Load an Env struct with variables from the OS environment.
 	e := &Env{}
-	es, err := env.UnmarshalFromEnviron(e)
+	envSet, err := env.EnvironToEnvSet(os.Environ())
+	if err != nil {
+		// Any errors here mean we're unstable, log and kill the app,
+		// preventing startup.  The app won't start listening until all
+		// dependencies report no errors so it is safe to do so immediately.
+		l.Fatal(err.Error())
+	}
+	// Overrides allow testing environment settings without global impact.
+	for _, override := range overrides {
+		envSet.Apply(override)
+	}
+	err = env.Unmarshal(envSet, e)
 	if err != nil {
 		l.Fatal(err.Error())
 	}
-	e.Extras = es
+	e.Extras = envSet
 
 	if e.Env == "" {
 		e.Env = DefaultEnv
@@ -70,7 +98,7 @@ func NewEnv() *Env {
 		e.Mode = Test
 	case "local":
 		e.Mode = Local
-	case "dev", "development", "stage", "staging", "prod", "production":
+	case "dev", "prod":
 		e.Mode = Release
 	}
 
